639fadbf86e922bf0c1e7d6f2cd44fb47b16a939
Fırat Köse <kose29.03@gmail.com>
2024-07-02 01:55:09 +0300

    demo deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ import Map from "./components/Map";
 export default function App() {
     return (
         <>
-            <section className={classes.homeSection}>
+            <header className={classes.navHeader}>
+                <div className={classes.logo} />
+                <nav className={classes.nav}>
+                    <a href='#about'>HAKKINDA</a>
+                    <a href='#map'>GALERİ</a>
+                    <a href='#contributors'>HAZIRLAYANLAR</a>
+                    <a href='https://www.bakirkoy.bel.tr/iletisim/iletisim-bilgileri.html' target='blank'>
+                        BİZE ULAŞIN
+                    </a>
+                </nav>
+            </header>
+            {/* <section className={classes.homeSection}>
                 <div className={classes.header}>
                     <h1>BAKIRKÖY'ÜN TARİHSEL COĞRAFYASI</h1>
                     <h4>Bakırköy'ün Araştırılması ve Haritalanması.</h4>
@@ -20,27 +31,26 @@ export default function App() {
                         </a>
                     </div>
                 </div>
-            </section>
-            <section id='about' style={{ height: "100vh" }}>
+            </section> */}
+            <section id='about' className={classes.aboutSection} style={{ height: "100vh" }}>
                 <div className={classes.about}>
-                    <h2>HAKKINDA</h2>
-                    <div className={classes.row}>
+                    {/* <h2>HAKKINDA</h2> */}
+                    <div className={classes.aboutTextContainer}>
                         <p>
                             Bu çalışma Bakırköy'ün tarihsel coğrafyasını (1453 - 1922) konu alan ve Osmanlı'nın son dönem haritaları üzerine
                             bu kültürel mirası göstermeyi hedefleyen bir dijital beşeri bilimler projesidir. Projenin hedefleri arasında
                             Bakırköyümüzün kültürel envanterini ortaya çıkartma ve kamuoyuna tanıtma amaçlarını gerçekleştirmek vardır. Bu
                             hedefler doğrultusunda bu uygulamanın oluşturulması söz konusu olmuş ve proje yürütülmüştür.
                         </p>
-                        <div className={classes.aboutImage} />
+                        {/* <div className={classes.aboutImage} /> */}
                     </div>
                 </div>
             </section>
-
             <section id='map' className={classes.mapSection}>
                 <Map />
             </section>
             <footer id='footer' className={classes.footer}>
-                {/* <div className={classes.footerLogo} /> */}
+                <div className={classes.footerLogo} />
                 <div id='contributors'>
                     <p className={`${classes.footerText} ${classes.footerTitle}`}>HAZIRLAYANLAR</p>
                     <p className={classes.footerText}>Burak KAYA - Tarihçi</p>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import { MapContainer, Marker, Popup, TileLayer, ImageOverlay } from "react-leaflet";
-import { latLngBounds, Icon } from "leaflet";
+import { latLngBounds, Icon, tooltip } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 import { Sidebar } from "primereact/sidebar";
 import { Divider } from "primereact/divider";
@@ -13,27 +14,74 @@ import { PanelMenu } from "primereact/panelmenu";
 import { Badge } from "primereact/badge";
 import { Button } from "primereact/button";
 import { Tooltip } from "primereact/tooltip";
+import { Galleria } from "primereact/galleria";
 
 import { IoChevronDown, IoChevronForward } from "react-icons/io5";
 import { RxQuestionMarkCircled } from "react-icons/rx";
+import { TbView360Number } from "react-icons/tb";
 
 import initialData from "./../data/data.json";
 
 import classes from "./Map.module.css";
 
-import bakirkoyMap from "./../assets/bakirkoy.jpg";
+import defaultIcon from "./../assets/icons/location.png";
 
-const icons = {
-    default: require("./../assets/icons/location.png"),
-    Kilise: require("./../assets/icons/church-location.png"),
-    Camii: require("./../assets/icons/mosque-location.png"),
-    Sinagog: require("./../assets/icons/synagogue-location.png"),
-    Okul: require("./../assets/icons/synagogue-location.png"),
-    Mezarlık: require("./../assets/icons/cemetery-location.png"),
-};
+import APLEH018A from "../assets/maps/APLEH018A.jpg";
+import APLEH062 from "../assets/maps/APLEH062.jpg";
+import Hrt_000432 from "../assets/maps/Hrt_000432.jpg";
+import Hrt_000433 from "../assets/maps/Hrt_000433.jpg";
+import Hrt_000434 from "../assets/maps/Hrt_000434.jpg";
+
+const maps = [
+    {
+        map: APLEH018A,
+        tooltip: "Makriköy ve Çevresi [1/25.000]",
+        bounds: [
+            [41.0505859, 28.7492441],
+            [40.9450737, 28.9128966],
+        ],
+        zoom: 13.1,
+    },
+    {
+        map: APLEH062,
+        tooltip: "Makriköy ve Çevresi [1/25.000]",
+        bounds: [
+            [41.0505859, 28.7492441],
+            [40.9450737, 28.9128966],
+        ],
+        zoom: 13.1,
+    },
+    {
+        map: Hrt_000432,
+        tooltip: "Ayastefanos [1/5.000]",
+        bounds: [
+            [40.9681227, 28.8095541],
+            [40.9526765, 28.8410442],
+        ],
+        zoom: 16,
+    },
+    {
+        map: Hrt_000433,
+        tooltip: "Makriköy'ün Kuzeyi [1/5.000]",
+        bounds: [
+            [40.9898254, 28.8628916],
+            [40.9766903, 28.8932927],
+        ],
+        zoom: 16,
+    },
+    {
+        map: Hrt_000434,
+        tooltip: "Makriköy'ün Güneyi [1/5.000]",
+        bounds: [
+            [40.979429, 28.8631314],
+            [40.9700284, 28.8865827],
+        ],
+        zoom: 16,
+    },
+];
 
 const filterItems = (itemRenderer) => [
-    { label: "İdari Yapı", template: itemRenderer },
+    { label: "İdari Yapılar", template: itemRenderer, items: [{ label: "İdari Yapı", template: itemRenderer }] },
     {
         label: "Sosyal Yapılar",
         template: itemRenderer,
@@ -60,7 +108,7 @@ const filterItems = (itemRenderer) => [
                         items: [
                             { label: "Ermeni Kilisesi", template: itemRenderer },
                             { label: "Rum Ortodoks Kilisesi", template: itemRenderer },
-                            { label: " Latin Katolik Kilisesi", template: itemRenderer },
+                            { label: "Latin Katolik Kilisesi", template: itemRenderer },
                         ],
                     },
                     { label: "Sinagog", template: itemRenderer },
@@ -162,17 +210,22 @@ const filterItems = (itemRenderer) => [
 ];
 
 export default function Map() {
+    const mapRef = useRef(null);
     const [selectedItem, setSelectedItem] = useState({});
     const [sideBarOpen, setSideBarOpen] = useState(false);
 
     const [yearInterval, setYearInterval] = useState([1453, 1922]);
     const [filterOutKeywords, setFilterOutKeywords] = useState([]);
 
-    const [showCemetery, setShowCemetery] = useState(true);
-    const [showChurch, setShowChurch] = useState(true);
+    const [activeMap, setActiveMap] = useState(maps[0]);
 
-    const bounds = new latLngBounds([41.0505859, 28.7492441], [40.9450737, 28.9128966]);
-    const mapbounds = new latLngBounds([41.0505859, 28.7492441], [40.9450737, 28.9128966]);
+    useEffect(() => {
+        mapRef?.current?.setView(
+            [(activeMap.bounds[0][0] + activeMap.bounds[1][0]) / 2, (activeMap.bounds[0][1] + activeMap.bounds[1][1]) / 2],
+            activeMap.zoom
+        );
+        mapRef?.current?.setMaxBounds(activeMap.bounds);
+    }, [activeMap]);
 
     const markerClickHandler = (item) => {
         setSelectedItem(item);
@@ -180,21 +233,6 @@ export default function Map() {
     };
 
     const iconSize = 32;
-
-    const checkboxes = [
-        {
-            name: "cemetery",
-            label: "Mezarlık",
-            checked: showCemetery,
-            onChange: setShowCemetery,
-        },
-        {
-            name: "church",
-            label: "Kilise",
-            checked: showChurch,
-            onChange: setShowChurch,
-        },
-    ];
 
     let data;
     data = initialData.filter((item) => {
@@ -233,27 +271,21 @@ export default function Map() {
             <div className={classes.sectionWrapper}>
                 <div className={classes.mapContainer}>
                     <div className={classes.mapActions}>
-                        <div className={classes.yearIntervalSliderContainer}>
-                            <Tooltip target='#yearIntervalInfo' />
-                            <div className='flex gap-2 align-items-center mb-2'>
-                                <div>{`${yearInterval[0]} - ${yearInterval[1]}`}</div>
+                        <div>
+                            <Tooltip target='.tooltipTarget' />
+                            <div className='flex gap-2 align-items-center mb-3'>
+                                <Chip label={`${yearInterval[0]} - ${yearInterval[1]}`} />
                                 <RxQuestionMarkCircled
-                                    id='yearIntervalInfo'
+                                    className='tooltipTarget'
                                     data-pr-tooltip='Haritada gösterilen yıl aralığı.'
                                     data-pr-position='right'
                                     data-pr-at='right+5 center'
                                     data-pr-my='left center-2'
                                 />
                             </div>
-                            <Slider
-                                className={classes.yearIntervalSlider}
-                                value={yearInterval}
-                                onChange={(e) => setYearInterval(e.value)}
-                                range
-                                min={1453}
-                                max={1922}
-                            />
+                            <Slider value={yearInterval} onChange={(e) => setYearInterval(e.value)} range min={1453} max={1922} />
                         </div>
+                        <Divider />
                         <div>
                             <div className='flex mb-2 gap-2'>
                                 <Button
@@ -271,68 +303,152 @@ export default function Map() {
                             </div>
                             <PanelMenu model={filterItems(itemRenderer)} className='w-full md:w-20rem flex flex-column gap-1' />
                         </div>
+
+                        <div className='flex gap-2'>
+                            <div className={classes.layersGroup}>
+                                <h6>Erkân-ı Harbiye-i Umumiye Matbaası, 1914-15</h6>
+                                <div className='flex gap-2'>
+                                    {maps.slice(0, 2).map((item, index) => (
+                                        <>
+                                            <Tooltip target='.tooltipTarget' />
+                                            <img
+                                                key={index}
+                                                src={item.map}
+                                                className={`${classes.layerItem} tooltipTarget`}
+                                                onClick={() => setActiveMap(maps[index])}
+                                                data-pr-tooltip={item.tooltip}
+                                                data-pr-position='bottom'
+                                            />
+                                        </>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className={classes.layersGroup}>
+                                <h6>Nedjib Bey Haritaları, 1918</h6>
+                                <div className='flex gap-2'>
+                                    {maps.slice(2).map((item, index) => (
+                                        <>
+                                            <Tooltip target='.tooltipTarget' />
+                                            <img
+                                                key={index}
+                                                src={item.map}
+                                                className={`${classes.layerItem} tooltipTarget`}
+                                                onClick={() => setActiveMap(maps[index + 2])}
+                                                data-pr-tooltip={item.tooltip}
+                                                data-pr-position='bottom'
+                                            />
+                                        </>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className={classes.mapWrapper}>
                         <MapContainer
+                            ref={mapRef}
                             style={{ width: "100%", height: "100%" }}
-                            center={[40.996426, 28.835127]}
-                            zoom={12.5}
+                            center={[
+                                (activeMap.bounds[0][0] + activeMap.bounds[1][0]) / 2,
+                                (activeMap.bounds[0][1] + activeMap.bounds[1][1]) / 2,
+                            ]}
+                            zoom={activeMap.zoom}
                             maxZoom={18}
-                            minZoom={12.5}
+                            minZoom={activeMap.zoom}
                             scrollWheelZoom={true}
-                            maxBounds={mapbounds}
+                            maxBounds={new latLngBounds(...activeMap.bounds)}
                             maxBoundsViscosity={1}
                         >
                             <TileLayer
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                             />
-                            {data.map((item, index) => {
-                                if (filterOutKeywords.includes(item.optimization)) return null;
-                                if (!item) return null;
-                                const position = item.location?.replace("\r", "").replace("\n", "").replace(" ", "").split(",").map(Number);
-                                if (!position) return null;
-                                return (
-                                    <Marker
-                                        key={index}
-                                        position={position}
-                                        icon={
-                                            new Icon({
-                                                // iconUrl: icons[item.category] || icons.default,
-                                                iconUrl: item.icon || icons.default,
-                                                iconSize: [iconSize, iconSize],
-                                                iconAnchor: [iconSize / 2, iconSize],
-                                                popupAnchor: [0, -iconSize],
-                                            })
-                                        }
-                                        eventHandlers={{
-                                            mouseover: (event) => event.target.openPopup(),
-                                            mouseout: (event) => event.target.closePopup(),
-                                            click: () => markerClickHandler(item),
-                                        }}
-                                    >
-                                        <Popup>{item.name}</Popup>
-                                    </Marker>
-                                );
-                            })}
-                            <ImageOverlay url={bakirkoyMap} bounds={bounds}></ImageOverlay>
+                            <MarkerClusterGroup>
+                                {data.map((item, index) => {
+                                    if (filterOutKeywords.includes(item.optimization)) return null;
+                                    if (!item) return null;
+                                    const position = item.location
+                                        ?.replace("\r", "")
+                                        .replace("\n", "")
+                                        .replace(" ", "")
+                                        .split(",")
+                                        .map(Number);
+                                    if (!position) return null;
+                                    return (
+                                        <Marker
+                                            key={index}
+                                            position={position}
+                                            icon={
+                                                new Icon({
+                                                    iconUrl: item.icon || defaultIcon,
+                                                    iconSize: [iconSize, iconSize],
+                                                    iconAnchor: [iconSize / 2, iconSize],
+                                                    popupAnchor: [0, -iconSize],
+                                                    className: classes.markerBg,
+                                                })
+                                            }
+                                            eventHandlers={{
+                                                mouseover: (event) => event.target.openPopup(),
+                                                mouseout: (event) => event.target.closePopup(),
+                                                click: () => markerClickHandler(item),
+                                            }}
+                                        >
+                                            <Popup>{item.name}</Popup>
+                                        </Marker>
+                                    );
+                                })}
+                            </MarkerClusterGroup>
+                            <ImageOverlay url={activeMap.map} bounds={new latLngBounds(...activeMap.bounds)}></ImageOverlay>
                         </MapContainer>
                     </div>
                 </div>
             </div>
             <Sidebar style={{ width: 600 }} visible={sideBarOpen} onHide={() => setSideBarOpen(false)}>
-                <img style={{ width: "100%", height: 200, objectFit: "contain" }} src={selectedItem.image?.split(",")[0]} />
+                <Galleria
+                    value={selectedItem.image?.split(",")}
+                    numVisible={5}
+                    style={{ maxWidth: "640px" }}
+                    item={(item) => <img src={item} style={{ width: "100%", height: "400px", objectFit: "contain" }} />}
+                    thumbnail={(item) => <img src={item} style={{ width: "60px", height: "60px", objectFit: "contain" }} />}
+                />
                 <Divider className='my-4' />
-                <h4>{selectedItem.name}</h4>
-                <Chip label={selectedItem.start_date} />
-                <Chip label={selectedItem.category} className='ml-2' />
-                <Accordion activeIndex={0} className='mt-4'>
-                    <AccordionTab header='Açıklama'>
-                        <p className='m-0' style={{ textAlign: "justify" }}>
-                            {selectedItem.description}
-                        </p>
-                    </AccordionTab>
-                </Accordion>
+                <div className='flex align-items-center justify-content-between'>
+                    <h4>{selectedItem.name}</h4>
+                    {selectedItem.street_view && (
+                        <div className='flex align-items-center gap-1'>
+                            <a href={selectedItem.street_view} target='blank'>
+                                <TbView360Number size={36} color='#4f5866' />
+                            </a>
+                            <Tooltip target='.tooltipTarget' />
+
+                            <RxQuestionMarkCircled
+                                className='tooltipTarget'
+                                data-pr-tooltip='Konumu 360° görmek için tıklayın.'
+                                data-pr-position='right'
+                                data-pr-at='right+5 center'
+                                data-pr-my='left center-2'
+                            />
+                        </div>
+                    )}
+                </div>
+                <div className='mb-2'>
+                    <Chip label={`${selectedItem.start_date} - ${selectedItem.end_date}`} />
+                    <Chip label={selectedItem.optimization} className='ml-2' />
+                </div>
+                <div className='mb-2'>
+                    <Chip label={`Şimdiki konum: ${selectedItem.current_local}`} />
+                </div>
+                <div>
+                    <Chip label={`Şimdiki durum: ${selectedItem.current_status}`} />
+                </div>
+                {selectedItem.description && (
+                    <Accordion activeIndex={0} className='mt-4'>
+                        <AccordionTab header='Açıklama'>
+                            <p className='m-0' style={{ textAlign: "justify" }}>
+                                {selectedItem.description}
+                            </p>
+                        </AccordionTab>
+                    </Accordion>
+                )}
             </Sidebar>
         </>
     );
